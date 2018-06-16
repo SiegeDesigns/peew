2e90ba0ea526cf3ed8ba809072e1d50b8f3d3187
sige <sigevvynckt@gmail.com>
2018-06-16 19:11:25 +0200

    Testpush

--- Peew/Assets/Scripts/PlayerController.cs
+++ Peew/Assets/Scripts/PlayerController.cs
@@ -8,23 +8,27 @@ public class Boundary
     public float xMin, xMax, zMin, zMax;
 }
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public float speed;
     public Boundary boundary;
 
     private Rigidbody2D rb;
     private float movement = 0f;
+    private Transform tf;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
+        tf = GetComponent<Transform>();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void FixedUpdate()
     {
@@ -34,17 +38,12 @@ public class PlayerController : MonoBehaviour {
         move.x = Input.GetAxis("Horizontal");
         move.y = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector2(move.x * speed, move.y * speed);
+        Vector3 pos = Camera.main.WorldToViewportPoint(tf.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        if (pos.x > 0 || pos.x < 1 || pos.y > 0 || pos.y < 0)
+            rb.velocity = new Vector2(move.x * speed, move.y * speed);
 
-        //float moveHorizontal = Input.GetAxis("Horizontal");
-        //float moveVertical = Input.GetAxis("Vertical");
 
-        //Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        //rb.velocity = movement * speed;      
-        //rb.position = new Vector2
-        //(
-        //    Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),            
-        //    Mathf.Clamp(rb.position.y, boundary.zMin, boundary.zMax)
-        //);
     }
 }
