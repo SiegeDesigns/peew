01eb3888a5723081f2ff312b729cc11ddd52d6ad
Jelle De Smit <jelle.desmit@student.kdg.be>
2018-06-17 16:15:46 +0200

    enemy animation when spawning + not spawning on player itself

--- Peew/Assets/Scripts/GameController.cs
+++ Peew/Assets/Scripts/GameController.cs
@@ -3,9 +3,11 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
-    public GameObject wave;
+    public GameObject greenEnemy;
+    public GameObject redEnemy;
     public float maxX, minX, maxY, minY;
     public int amountOfEnemies;
 
@@ -14,45 +16,37 @@ public class GameController : MonoBehaviour {
     public int score;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         scoreText = GameObject.FindGameObjectWithTag("Score Text").GetComponent<Text>();
         score = 0;
         UpdateScore();
         SpawnWaves();
-	}
+    }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
+    void Update()
+    {
+
+    }
 
     //spawns waves of enemies
     void SpawnWaves()
     {
-        Debug.Log("Screen width: " + Screen.width);
-        Debug.Log("Screen height: " + Screen.height);
-        List<Vector3> enemyList = new List<Vector3>();
+        bool overlap = false;
         Vector3 spawnPosition = new Vector3();
-        bool overlap;
 
         for (int i = 0; i < amountOfEnemies; i++)
         {
-            //Random position to spawn at
-            //if(enemyList.Count == 0) spawnPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 1);
-            //else
-            //{
-            //    spawnPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 1);
-            //    overlap = Physics2D.OverlapCircle(spawnPosition, 0.1f);
-
-            //    while (overlap)
-            //    {
-            //        spawnPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 1);
-            //        overlap = Physics2D.OverlapArea(enemyList[i], spawnPosition);
-            //    }
-            //}
             spawnPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 1);
+            overlap = Physics2D.OverlapCircle(spawnPosition, 1);
+            while (overlap)
+            {
+                spawnPosition = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 1);
+                overlap = Physics2D.OverlapCircle(spawnPosition, 0.5f);
+            }
             Quaternion spawnRotation = Quaternion.Euler(0, 0, 0);
-            Instantiate(wave, spawnPosition, spawnRotation);
+            Instantiate(greenEnemy, spawnPosition, spawnRotation);
         }
     }
 
